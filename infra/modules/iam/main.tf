a26efcb11e2fb228cd19ee8937e9204d6484ea02
Said <said.hajisaid@hotmail.com>
2026-08-18 21:20:35 +0100

    Fix OIDC trust policy for GitHub immutable sub claim

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -20,7 +20,7 @@ resource "aws_iam_role" "github_actions" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
+          "token.actions.githubusercontent.com:sub" = "repo:saidspace@141554643/aws-ecs-project@1335321965:*"
         }
       }
     }]
